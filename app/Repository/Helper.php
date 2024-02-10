9cdd448e945640e086b0344c4f4b3a5747927480
Rahat Hossain <webmaazgmail.com>
2024-02-10 22:07:52 +0600

    login file added

---
+++ app/Repository/Helper.php
@@ -0,0 +1,7 @@
+<?php 
+
+    // if(!function_exists('get_admin_url')){
+    //     function get_admin_url(){
+    //         return 'auth';
+    //     }
+    // }
--- routes/admin.php
+++ routes/admin.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 
+
+// $prefix = get_admin_url();
+
+
 Route::prefix('auth')->group(function () {
     Route::name('auth.')->group(function () {
         Route::get('/login', [AuthController::class, 'login'])->name('login');
     });
 });
+
