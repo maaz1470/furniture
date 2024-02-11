1274f8d22d7e54c816724a54355f4b10786d62e5
Rahat Hossain <webmaazgmail.com>
2024-02-11 17:44:12 +0600

    update

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import Home from "./Page/Home";
+import Home from "./front-end/Page/Home";
 
 
 console.log('something')
--- resources/js/backend/app.jsx
+++ resources/js/backend/app.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { AdminURL } from "./hook/useAdminUrl";
+import Main from "./Layout/Main";
+import Login from "./Pages/Auth/Login";
+
+
+const router = createBrowserRouter([
+    {
+        path: AdminURL,
+        element: <Main />,
+        children: [
+            {
+                path: '',
+                element: <Login />
+            }
+        ]
+    }
+])
+
+
+
 
 
 if(document.getElementById('app')){
     const root = ReactDOM.createRoot(document.getElementById('app'));
     root.render(
         <React.StrictMode>
-            <h1>Hello Something</h1>
+            <RouterProvider router={router}></RouterProvider>
         </React.StrictMode>
     )
 }
