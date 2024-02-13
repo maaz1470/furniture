75f5e9855e88694a504ebd68c9104bd09e262dcb
imadnanhassan <abulhassn.dev@gmail.com>
2024-02-13 21:11:01 +0600

    route

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
-import Home from "./Page/Home";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import route from "./front-end/route";
 
-
-console.log('something')
-
-if(document.getElementById('root')){
-    const root = ReactDOM.createRoot(document.getElementById('root'))
+if (document.getElementById("root")) {
+    const root = ReactDOM.createRoot(document.getElementById("root"));
     root.render(
         <React.StrictMode>
-            <Home />
+            <RouterProvider router={route} />
         </React.StrictMode>
-    )
+    );
 }
---
+++ resources/js/front-end/layout/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../shared/Header";
+import Footer from "../shared/Footer";
+
+export default function MainLayout() {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+}
